e7d43aa53eede534dd70968c4e6e5f1a0eefb394
Flcog01 <flcorralesg01@iescastelar.com>
2024-04-02 16:05:31 +0200

    bug boton arreglado

--- proyecto java/src/App.java
+++ proyecto java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Botón antiguo");
+        System.out.println("Botón funcionando");
     }
 }
